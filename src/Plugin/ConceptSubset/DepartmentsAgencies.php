48d18c9dc8dfb82568f9510d98a0342b5f5a0a0c
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2020-10-22 15:43:51 +0300

    EWPP-413: Add concept subset for content owner field.

---
+++ src/Plugin/ConceptSubset/DepartmentsAgencies.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_corporate_site_info\Plugin\ConceptSubset;
+
+use Drupal\Core\Entity\Query\QueryInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\rdf_entity\RdfFieldHandlerInterface;
+use Drupal\rdf_skos\ConceptSubsetPluginBase;
+use Drupal\rdf_skos\Plugin\PredicateMapperInterface;
+
+/**
+ * Creates a subset of the corporate bodies vocabulary.
+ *
+ * @ConceptSubset(
+ *   id = "oe_corporate_site_info_corporate_bodies_department_executive_agencies",
+ *   label = @Translation("Departments & Executive Agencies"),
+ *   description = @Translation("Includes DGs, executive agencies and service departments."),
+ *   predicate_mapping = TRUE,
+ *   concept_schemes = {
+ *     "http://publications.europa.eu/resource/authority/corporate-body"
+ *   }
+ * )
+ */
+class DepartmentsAgencies extends ConceptSubsetPluginBase implements PredicateMapperInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterQuery(QueryInterface $query, $match_operator, array $concept_schemes = [], string $match = NULL): void {
+    $types = [
+      // Directorate-general.
+      'http://publications.europa.eu/resource/authority/corporate-body-classification/DIR_GEN',
+      // Service departments.
+      'http://publications.europa.eu/resource/authority/corporate-body-classification/SERV_DEP',
+      // Executive agency.
+      'http://publications.europa.eu/resource/authority/corporate-body-classification/AGENCY_EXEC',
+    ];
+    $query->condition('ewcms_corporate_body_classification', $types, 'IN');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPredicateMapping(): array {
+    $mapping = [];
+
+    $mapping['ewcms_corporate_body_classification'] = [
+      'column' => 'target_id',
+      'predicate' => ['http://purl.org/dc/terms/type'],
+      'format' => RdfFieldHandlerInterface::RESOURCE,
+    ];
+
+    return $mapping;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getBaseFieldDefinitions(): array {
+    $fields = [];
+
+    $fields['ewcms_corporate_body_classification'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Corporate Bodies Classification'))
+      ->setDescription(t('The corporate body classification.'))
+      ->setSetting('target_type', 'skos_concept')
+      ->setCardinality(FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED)
+      ->setSetting('handler', 'default')
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'entity_reference_label',
+      ]);
+
+    return $fields;
+  }
+
+}
