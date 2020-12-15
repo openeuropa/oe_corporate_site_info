4d10f5c47f2aa5f9651a7c57bffab0274aeaca1b
Sergei Pavlenko <sergii.pavlenko.v@gmail.com>
2020-12-15 11:28:02 +0200

    EWPP-546: Update post_update function name.

--- oe_corporate_site_info.post_update.php
+++ oe_corporate_site_info.post_update.php
@@ -10,6 +10,6 @@ declare(strict_types = 1);
 /**
  * Install Multi-value form element contrib module.
  */
-function oe_corporate_site_info_post_update_install_multivalue_form_element_module(): void {
+function oe_corporate_site_info_post_update_00001(): void {
   \Drupal::service('module_installer')->install(['multivalue_form_element']);
 }
